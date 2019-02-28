93dd442e2c8058c9353447417a71e5996ed449e9
cowardlycrow <45370177+cowardlycrow@users.noreply.github.com>
2019-02-28 13:50:28 -0500

    Delete secure-ssh.sh

--- linux/centos7/secure-ssh.sh
+++
@@ -1,7 +0,0 @@
-# SECURE-SSH.SH
-# AUTHOR COWARDLYCROW
-# Creates a new ssh user called $1
-# Adds a public key to that users authorized keys file
-# Removes roots ability to ssh in
-
-echo "testing 123"
